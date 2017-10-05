36545c479d97cfb0d70d87ea955c8fed330d8c65
chiwawaweb <git@chiwawaweb.com>
2017-10-05 13:07:31 +0200

    Héritage

--- GrcEvo.DTO/EntityTiers.cs
+++ GrcEvo.DTO/EntityTiers.cs
@@ -9,5 +9,25 @@ namespace GrcEvo.DTO
     public class EntityTiers
     {
         public int ID { get; set; }
+        public string PrefixCode { get; set; }
+        public int NumberCode { get; set; }
+        public string Company { get; set; }
+        public string Civility { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Adress1 { get; set; }
+        public string Adress2 { get; set; }
+        public string Adress3 { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PhoneNumber { get; set; }
+        public string GsmNumber { get; set; }
+        public string FaxNumber { get; set; }
+        public string Email { get; set; }
+        public string Web { get; set; }
+        public string Note { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
     }
 }
